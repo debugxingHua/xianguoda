4b30005ea77aa96bf142ec4eca847560f14b8d88
ynn <373098773@qq.com>
2020-09-28 22:23:25 +0800

    鲜果达(小程序)-修改测试

--- xgd-smallsoft/pages/index/index.js
+++ xgd-smallsoft/pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello World222222',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
